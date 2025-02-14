e9656e318c2e324f849b4eddd7f5ad4344d29179
scottqueen-bixal <scott.queen@bixal.com>
2025-02-14 15:52:32 -0500

    tweak blob

--- src/lib/components/Blob.svelte
+++ src/lib/components/Blob.svelte
@@ -5,14 +5,14 @@
   let currentNoiseMultiplier = noiseMultiplier;
 
   const noiseMax = .2;
-  const blobSize = 50;
+  const blobSize = 300;
   const rotationSpeed = 0.0095;
 
   const sketch = (p5: any) => {
     let phase = 0;
 
     p5.setup = () => {
-      p5.createCanvas(blobSize + 75, blobSize + 75, p5.WEBGL);
+      p5.createCanvas(blobSize + 400, blobSize + 400, p5.WEBGL);
       p5.smooth();
       p5.pixelDensity(1);
     };
@@ -27,8 +27,10 @@
       p5.scale(currentScale);
 
       for (let lat = 0; lat <= 180; lat += 4) {
-        for (let lon = 0; lon <= 360; lon += 4) {
+        p5.push();
+        p5.rotateZ(p5.frameCount * rotationSpeed * (lat / 180));
 
+        for (let lon = 0; lon <= 360; lon += 4) {
           const r = blobSize + p5.map(
             p5.noise(
               p5.sin(p5.radians(lon)) * noiseMax * currentNoiseMultiplier,
@@ -49,9 +51,10 @@
           p5.translate(x, y, z);
           p5.noStroke(0);
           p5.fill(20, 200);
-          p5.sphere(10);
+          p5.sphere(300);
           p5.pop();
         }
+        p5.pop();
       }
 
       phase += 0.03;
@@ -65,8 +68,8 @@
 
 <style>
   .canvas-container {
-    width: 100px;
-    height: 100px;
+    width: 400px;
+    height: 400px;
     margin: 0 auto;
   }
 </style>
